5f36a235ef63b992e88a09c4dfb4454e907ac644
Dtchmnt <dtchmnt.ishim@gmail.com>
2021-07-06 19:44:25 +0700

    paste migration

---
+++ database/migrations/2021_07_06_123759_create_paste_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePasteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pastes', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('link')->unique();
+            $table->string('name');
+            $table->string('title');
+            $table->text('text');
+            $table->tinyInteger('privacy');
+            $table->tinyInteger('expiration');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paste');
+    }
+}
